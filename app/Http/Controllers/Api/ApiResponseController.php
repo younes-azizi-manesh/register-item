1f25b798a5c2118a955fe4dc0f494a33d9cfe586
younes <younesazizimanesh@gmail.com>
2025-07-12 11:09:01 +0330

    Refactor ApiResponseController and ItemService

--- app/Http/Controllers/Api/ApiResponseController.php
+++ app/Http/Controllers/Api/ApiResponseController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\ItemResource;
 
 class ApiResponseController extends Controller
 {
     public function successResponse(mixed $data, string $status, string $message, int $statusCode = 200)
     {
         $result = [];
-        $result['data'] = new ItemResource($data);
+        $result['data'] = $data;
         $result['status'] = $status;
         $result['message'] = $message;
         return response()->json($result, $statusCode);
